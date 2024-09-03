c1bbcba465d9ffd7bbb727a6643ce4a6025ff685
Matteo Rossi <matteoroxis@gmail.com>
2024-09-03 10:36:08 +0200

    First commit

---
+++ src/main/java/it/matteoroxis/liquibase_explorer/LiquibaseExplorerApplication.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.liquibase_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/matteoroxis/liquibase_explorer/LiquibaseExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.matteoroxis.liquibase_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
